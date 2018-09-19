9b3632a9bd2783a059794fb08ab4b7e14eb48460
jstarczewski <jan.a.starczewski@gmail.com>
2018-09-19 10:56:51 +0200

    Sensors package created

---
+++ app/src/main/java/com/clakestudio/pc/airpollutionmonitoringapp/sensors/SesnsorsListActivity.java
@@ -0,0 +1,13 @@
+package com.clakestudio.pc.airpollutionmonitoringapp.sensors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SesnsorsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sesnsors_list);
+    }
+}
